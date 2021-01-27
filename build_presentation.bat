71eba10058122604e8bba0efd7f85b6d6706bfbf
donaldwu <donaldwu@tencent.com>
2021-01-27 10:11:34 +0800

    explicitly specify revealjs-url

--- build_presentation.bat
+++ build_presentation.bat
@@ -1,3 +1,3 @@
 cd %~dp0
 
-pandoc README.md -t revealjs -s --slide-level 3 -V slideNumber=true --template=template/default.html -V theme=blood -V transition=slide --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML -o README.html
+pandoc README.md -t revealjs -s --slide-level 3 -V revealjs-url=reveal.js -V slideNumber=true --template=template/default.html -V theme=blood -V transition=slide --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML -o README.html
--- build_presentation.sh
+++ build_presentation.sh
@@ -7,4 +7,4 @@ CURRENT=`pwd`
 BASENAME=`basename "$CURRENT"`
 
 
-pandoc README.md -t revealjs -s --slide-level 3 -V slideNumber=true --template=template/default.html -V theme=blood -V transition=slide --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML -o README.html
+pandoc README.md -t revealjs -s --slide-level 3 -V revealjs-url=reveal.js -V slideNumber=true --template=template/default.html -V theme=blood -V transition=slide --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML -o README.html
